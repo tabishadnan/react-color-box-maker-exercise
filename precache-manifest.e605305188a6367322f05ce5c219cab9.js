8a8e957e1c023584d913f2c068c41c22da0e8f14
tabishadnan <tabishadnan000@gmail.com>
2020-12-24 00:30:33 +0500

    Updates

---
+++ precache-manifest.e605305188a6367322f05ce5c219cab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543075f33d2e0754c2d26b98e45d2ef4",
+    "url": "/react-color-box-maker-exercise/index.html"
+  },
+  {
+    "revision": "dc21c5a68c8d7ff43faf",
+    "url": "/react-color-box-maker-exercise/static/css/main.9c52340c.chunk.css"
+  },
+  {
+    "revision": "f1ab9b9afa02eccb7c75",
+    "url": "/react-color-box-maker-exercise/static/js/2.a1f3f4c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-color-box-maker-exercise/static/js/2.a1f3f4c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc21c5a68c8d7ff43faf",
+    "url": "/react-color-box-maker-exercise/static/js/main.10926a2e.chunk.js"
+  },
+  {
+    "revision": "6e80d7c401fbac41c06f",
+    "url": "/react-color-box-maker-exercise/static/js/runtime-main.2e4fcc15.js"
+  }
+]);
